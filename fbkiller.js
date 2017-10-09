ff7296f81fdb30cbafdfb5863ce641e27355f2f5
Justin Collier <justincollier217@gmail.com>
2017-10-09 12:01:40 -0500

    add linked in feed

--- fbkiller.js
+++ fbkiller.js
@@ -1,2 +1,14 @@
-var elem = document.querySelectorAll('[aria-label="News Feed"]');
-elem[0].style.visibility = "hidden";
+// collect fields
+var newsfeed = document.querySelectorAll('[aria-label="News Feed"]');
+var linkedinfeed = document.getElementByClassName('core-rail');
+
+// hide fb feed if present
+if(newsfeed.length != 0) {
+  newsfeed[0].style.visibility = "hidden";
+}
+
+// hide linkedin feed if present
+if(linkedinfeed.length != 0) {
+  linkedinfeed[0].style.visibilty = "hidden";
+}
+
